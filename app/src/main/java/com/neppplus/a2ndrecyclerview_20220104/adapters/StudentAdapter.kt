00f68aaf9394adc1cee5cd53f8d28d8ae47be0a8
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 13:01:41 +0900

    4-6. onCreateViewHolder : 어떤 xml을 보여줄래? inflate 작업 후 MyViewHolder에 담아서 리턴

--- app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.a2ndrecyclerview_20220104.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.a2ndrecyclerview_20220104.R
 import com.neppplus.a2ndrecyclerview_20220104.models.Student
 
 class StudentAdapter(
@@ -18,6 +20,14 @@ class StudentAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+//        어떤 xml을 보여줄거야?
+//        1 : 한 줄을 표현하는 xml(student_list_item)을 가지고, inflate(?? xml에 표기된 레이아웃들을 메모리에 로딩된 후 객체화 시키는 과정)
+//        쉽게 말하면, layout에 그때 그때 다른 layout을 집어 넣을 수 있는 작업
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, parent, false)
+
+//        2 : 한 줄을 MyViewHolder에 담아서 리턴
+        return MyViewHolder(row)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
