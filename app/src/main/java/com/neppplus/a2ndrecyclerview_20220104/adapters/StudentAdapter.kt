0d308ba6de41c42d028a98d46e913acc10501409
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 12:50:55 +0900

    4-3. StudentAdapter에 커서 두고, alt + enter로 3개 함수 모두 불러냄

--- app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
@@ -1,5 +1,7 @@
 package com.neppplus.a2ndrecyclerview_20220104.adapters
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>() {
@@ -7,5 +9,17 @@ class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>() {
     inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
 
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
