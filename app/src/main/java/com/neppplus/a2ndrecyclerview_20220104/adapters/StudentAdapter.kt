8abb0b59388291e6237ad2d0892b9e5931801188
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 12:49:43 +0900

    4-2. inner class로 MyViewHolder를 생성하고, RecyclerView.ViewHolder를 상속받게함 그다음, inner class의 생성자를 수정해서 row:View를 받아, 부모 클래스에게 넘겨줌 StudentAdapter의 <>내부에 내부 클래스로 완성한 MyViewHoler 넣어주기

--- app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/a2ndrecyclerview_20220104/adapters/StudentAdapter.kt
@@ -2,5 +2,10 @@ package com.neppplus.a2ndrecyclerview_20220104.adapters
 
 import androidx.recyclerview.widget.RecyclerView
 
-class StudentAdapter : RecyclerView.Adapter<>() {
+class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(row: View) : RecyclerView.ViewHolder(row) {
+
+
+    }
 }
